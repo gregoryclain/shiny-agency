054e6136f552ea31aec1f5235baa2ce15885ea8c
Grégory Clain <g.clain69@gmail.com>
2022-06-05 19:49:46 +0200

    add styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,21 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import colors from '../../utils/style/colors';
+
+const StyledLink = styled(Link)`
+  padding: 15px;
+  color: #8186a0;
+  text-decoration: none;
+  font-size: 18px;
+  ${(props) => props.$isFullLink && `color: white; border-radius: 30px; background-color:${colors.primary};`}
+`
 
 function Header() {
   return (
     <nav>
-      <Link to="/">Accueil</Link>
-      <Link to="/freelances">Freelance</Link>
-      <Link to="/survey/42">Questionnaire</Link>
+      <StyledLink to="/">Accueil</StyledLink>
+      <StyledLink to="/freelances">Freelance</StyledLink>
+      <StyledLink to="/survey/1" $isFullLink>Questionnaire</StyledLink>
     </nav>
   );
 }
